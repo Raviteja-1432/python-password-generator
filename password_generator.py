cdf1b13d6f0631fceb80f48188f6b95adcce71ed
R-a-v-i-t-e-j-a <115697309+R-a-v-i-t-e-j-a@users.noreply.github.com>
2022-10-13 14:10:52 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,27 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+A=[]
+B=[]
+C=[]
+a = random.sample(letters, nr_letters)
+for i in a:
+  A.append(i)
+b = random.sample(numbers, nr_numbers)
+for i in b:
+  B.append(i)
+c = random.sample(symbols, nr_symbols)
+for i in c:
+  C.append(i)
+password=A+B+C
+random.shuffle(password)
+print("The passowrd is: ",end="")
+for i in password:
+  print(i,end="")
+print("/n")
